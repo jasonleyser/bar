2ebdeb2912e36b071706200462f2bb0abfb8c012
jasonleyser <60402678+jasonleyser@users.noreply.github.com>
2020-12-24 22:20:43 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -120,7 +120,7 @@ async function refreshData() {
 }
 
 var x = setInterval(function () {
-  var countDownDate = new Date("Dec 24, 2020 21:00:00").getTime();
+  var countDownDate = new Date("Dec 25, 2020 07:30:00").getTime();
 
   // Get today's date and time
   var now = new Date().getTime();
